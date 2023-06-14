b03c77a651932d7622c1d1d5bd06e37f757e9be1
yhl <yuhl@jvtd.cn>
2023-06-14 14:33:55 +0800

    【yhl】feat:添加tdesign组件库

--- src/main.js
+++ src/main.js
@@ -2,22 +2,24 @@
  * @Author: yhl
  * @Date: 2023-05-18 20:15:22
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-06-08 17:05:21
+ * @LastEditTime: 2023-06-09 14:51:11
  * @FilePath: /low-code-js/src/main.js
  */
 import { createApp } from 'vue'
 import './style/index.css'
-import ArcoVue from '@arco-design/web-vue';
-import ArcoVueIcon from '@arco-design/web-vue/es/icon';
-import '@arco-design/web-vue/dist/arco.css';
 import router from './router'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue'
+import ArcoVueIcon from '@arco-design/web-vue/es/icon'
+import '@arco-design/web-vue/dist/arco.css'
+import TDesign from 'tdesign-mobile-vue'
 
 const app = createApp(App)
 
 app.use(ArcoVue)
 app.use(ArcoVueIcon)
+app.use(TDesign)
 app.use(router)
 app.use(createPinia())
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
  * @Author: yhl
  * @Date: 2023-06-06 14:16:19
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-06-08 17:25:07
+ * @LastEditTime: 2023-06-12 16:40:22
  * @FilePath: /low-code-js/src/views/Home.vue
 -->
 <script setup>
